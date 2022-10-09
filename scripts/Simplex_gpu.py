323825bbf371f803483f1bc2df71fe8aefe59269
JuanRomero <juan.romeromurcia@kaust.edu.sa>
2022-10-09 09:32:09 +0300

    Cleaning up notebook

--- scripts/Simplex_gpu.py
+++ scripts/Simplex_gpu.py
@@ -1,24 +1,20 @@
-import logging
 import numpy as np
-from pyproximal.ProxOperator import _check_tau
 from pyproximal import ProxOperator
-from pyproximal.projection import SimplexProj
-import numba
-from numba import cuda
 from scripts.bisection_gpu import *
-
 
 
 class _Simplex_numba_gpu(ProxOperator):
     """Simplex operator (numba version)
    """
+
     def __init__(self, n, radius, dims=None, axis=-1,
-                 maxiter=100, ftol=1e-8, xtol=1e-8, call=False):
+                 maxiter=100, ftol=1e-8, xtol=1e-8, call=True):
         super().__init__(None, False)
         if dims is not None and len(dims) != 2:
             raise ValueError('provide only 2 dimensions, or None')
         self.n = n
-        self.coeffs = np.ones(self.n if dims is None else dims[axis]).todevice()
+        # self.coeffs = cuda.to_device(np.ones(self.n if dims is None else dims[axis]))
+        self.coeffs = np.ones(self.n if dims is None else dims[axis])
         self.radius = radius
         self.dims = dims
         self.axis = axis
@@ -32,25 +28,22 @@ class _Simplex_numba_gpu(ProxOperator):
         x = x.reshape(self.dims)
         if self.axis == 0:
             x = x.T
-            
+
         num_threads_per_blocks = 32
         num_blocks = (x.shape[0] + num_threads_per_blocks - 1) // num_threads_per_blocks
-        # y = np.empty_like(x)
-        y = cuda.device_array_like(x)
-        
-        simplex_jit_gpu[num_blocks,num_threads_per_blocks](x, self.coeffs, self.radius, 0, 10000000000, self.maxiter, self.ftol, self.xtol, y)
+        y = np.empty_like(x)
+        # y = cuda.device_array_like(x)
 
-        # y = simplex_jit_gpu(x, self.coeffs, self.radius, 0, 10000000000,
-        #                 self.maxiter, self.ftol, self.xtol)
-        
+        simplex_jit_gpu[num_blocks, num_threads_per_blocks](x, self.coeffs, self.radius, 0, 10000000000, self.maxiter,
+                                                            self.ftol, self.xtol, y)
+
         if self.axis == 0:
             y = y.T
         return y.ravel()
 
 
 def Simplex_gpu(n, radius, dims=None, axis=-1, maxiter=100,
-            ftol=1e-8, xtol=1e-8, call=True, engine='numpy'):
-    
+                ftol=1e-8, xtol=1e-8, call=True):
     s = _Simplex_numba_gpu(n, radius, dims=dims, axis=axis,
-                   maxiter=maxiter, ftol=ftol, xtol=xtol, call=call)
+                           maxiter=maxiter, ftol=ftol, xtol=xtol, call=call)
     return s
